cca0755694eddfca9433e734443a95480be9e9cf
IgorKirilenko <abromssik@gmail.com>
2022-04-03 12:45:22 +0300

    for debian

--- packages_install/install.sh
+++ packages_install/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-cp /root/packages_installed /tmp/packages_installed
+cp /root/bash/packages_install/packages_installed /tmp/packages_installed
 line=$(wc -l /tmp/packages_installed|cut -c -2)
 while [ $line -gt 0 ]
 do
 package=$(sed -n 1p /tmp/packages_installed)
-yum install -y $package
+#добавь для debian c if
+apt install -y $package
 sed -i 1d /tmp/packages_installed
 line=$(wc -l /tmp/packages_installed|cut -c -2)
 done
